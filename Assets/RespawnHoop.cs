cbcd69f5645d455a406f558a4514e6a7a83351fb
misumeme <kazyproductions@gmail.com>
2024-04-11 08:11:53 -0500

    Minor adjustments to various things

---
+++ Assets/RespawnHoop.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnHoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject torus_prefab;
+
+    private void OnCollisionEnter(Collision collision) {
+        //Another object touches torus
+        //collision is the collider that has collided with us
+        Debug.Log(collision.gameObject.name + "has collided with the nose");
+        Instantiate(torus_prefab,transform.position,transform.rotation);
+    }
+}
